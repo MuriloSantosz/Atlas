05798bad7ae11286026bdc41a474043bee882e3d
murilo <murilosantosint@gmail.com>
2026-08-27 19:49:04 -0300

    --

--- app/src/main/java/com/atlasapp/atlas/ui/telas/CadastroResponsavel.kt
+++ app/src/main/java/com/atlasapp/atlas/ui/telas/CadastroResponsavel.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -26,6 +27,7 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -213,6 +215,7 @@ fun CadastroResponsavel(modifier: Modifier = Modifier, navController: NavControl
                             modifier = Modifier.size(25.dp)
                         )
                     },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                     colors = TextFieldDefaults.colors(
                         focusedContainerColor = BrancoAtlas,
                         unfocusedContainerColor = BrancoAtlas,
